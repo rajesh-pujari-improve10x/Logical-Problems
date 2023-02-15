180e6f43ab7822df47143a5e0727b85ca314c6dc
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-02-15 11:10:45 +0530

    I implemented Exception functionality in NameGreetingControllerImpl

--- app/src/main/java/com/improve10x/logicalproblems/namegreeting/NameGreetingControllerImpl.java
+++ app/src/main/java/com/improve10x/logicalproblems/namegreeting/NameGreetingControllerImpl.java
@@ -3,7 +3,11 @@ package com.improve10x.logicalproblems.namegreeting;
 public class NameGreetingControllerImpl implements NameGreetingController {
     @Override
     public String strNameGreeting(String name) {
-        String greetingName = "Hello " + name + "!";
-        return greetingName;
+        try {
+            String greetingName = "Hello " + name + "!";
+            return greetingName;
+        } catch (Exception ex) {
+            return "";
+        }
     }
 }
