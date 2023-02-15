2d97344b4d74a4d8b0e521d47a6c7951c6e99f09
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-02-15 10:34:28 +0530

    I implemented Exception functionality in HrsAndMinsToSecondsControllerImpl

--- app/src/main/java/com/improve10x/logicalproblems/hoursandminutesintoseconds/HrsAndMinsToSecondsControllerImpl.java
+++ app/src/main/java/com/improve10x/logicalproblems/hoursandminutesintoseconds/HrsAndMinsToSecondsControllerImpl.java
@@ -3,10 +3,14 @@ package com.improve10x.logicalproblems.hoursandminutesintoseconds;
 public class HrsAndMinsToSecondsControllerImpl implements HrsAndMinsToSecondsController {
     @Override
     public int convertHrsAndMinsToSeconds(String hours, String minutes) {
-        int hrs = Integer.parseInt(hours);
-        int mins = Integer.parseInt(minutes);
-        int seconds = hrs * 60 * 60;
-        int sec = mins * 60;
-        return seconds + sec;
+        try {
+            int hrs = Integer.parseInt(hours);
+            int mins = Integer.parseInt(minutes);
+            int seconds = hrs * 60 * 60;
+            int sec = mins * 60;
+            return seconds + sec;
+        } catch (Exception ex) {
+            return -1;
+        }
     }
 }
