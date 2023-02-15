134628a213e6bf7e4f16bb44a5e72ab104b4d7b5
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-02-15 10:45:08 +0530

    I implemented Exception functionality in MinsToSecondsControllerImpl

--- app/src/main/java/com/improve10x/logicalproblems/minutestoseconds/MinsToSecondsControllerImpl.java
+++ app/src/main/java/com/improve10x/logicalproblems/minutestoseconds/MinsToSecondsControllerImpl.java
@@ -3,8 +3,12 @@ package com.improve10x.logicalproblems.minutestoseconds;
 public class MinsToSecondsControllerImpl implements MinsToSecondsController {
     @Override
     public int convertMinsToSeconds(String minutes) {
-        int mins = Integer.parseInt(minutes);
-        int seconds = mins * 60;
-        return seconds;
+        try {
+            int mins = Integer.parseInt(minutes);
+            int seconds = mins * 60;
+            return seconds;
+        } catch (Exception ex) {
+            return -1;
+        }
     }
 }
